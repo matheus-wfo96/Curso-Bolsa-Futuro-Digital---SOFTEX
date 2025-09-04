608d780ecf4df3770c16a1c2f677e86a77485443
Matheus Wilker <matheus.pcpb@gmail.com>
2025-09-03 23:37:21 -0300

    Add files via upload

---
+++ atividade_30questões_curso.py
@@ -0,0 +1,50 @@
+
+#QUESTÃO 1
+nome = "Matheus Wilker"
+print(nome)
+
+#QUESTÃO 2
+nome=input("Qual seu nome? ")
+idade=input("Qual sua idade? ")
+print("Olá", nome,"","", "você tem", idade, "anos")
+
+# QUESTÃO 3
+cidade="Campina_Grande"
+estado="Paraíba"
+print("Mono na cidade de", (cidade), "-", (estado))
+
+#QUESTÃO 4
+curso=input("QUal curso você escolhe? ")
+print("Você escolheu o curso de", (curso),".")
+
+#QUESTÃO 5
+filme="O Auto da Compadecida"
+ano="2002"
+print('O Filme', (filme), "foi lançado em ", (ano))
+
+#QUESTÃO 6
+numero1=int(input("Escolha o pirmieor número "))
+numero2=int(input("Escolha o segundo número "))
+print("A soma dos dois números é: ", (numero1) + (numero2))
+
+#QUESTÃO 7
+numero1=int(input("Escolha o pirmieor número "))
+numero2=int(input("Escolha o segundo número "))
+print("A subatração dos dois números é: ", (numero1) - (numero2))
+
+#QUESTÃO 8
+numero1=int(input("Escolha o pirmieor número "))
+numero2=int(input("Escolha o segundo número "))
+print("A multiplicaçãp dos dois números é: ", (numero1) * (numero2))
+
+#QUESTÃO 9 
+numero1=int(input("Escolha o pirmieor número "))
+numero2=int(input("Escolha o segundo número "))
+print("A divisão dos dois números é: ", (numero1) / (numero2))
+
+#QUESTÃO 10
+numero=int(input("Escolha um número "))
+print("O dobro de ", (numero), "é ", (numero) * 2)
+
+
+
