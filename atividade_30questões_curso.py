97197e9f7457a8894fbd3bbf5b1ba7a3a547413e
Matheus Wilker <matheus.pcpb@gmail.com>
2025-09-04 23:44:10 -0300

    Update atividade_30questões_curso.py
    atividade iniciais de de variáveis em python - finalizada em 04/08/25

--- atividade_30questões_curso.py
+++ atividade_30questões_curso.py
@@ -46,5 +46,114 @@ print("A divisão dos dois números é: ", (numero1) / (numero2))
 numero=int(input("Escolha um número "))
 print("O dobro de ", (numero), "é ", (numero) * 2)
 
+#QUESTÃO 11
+nota1=int(input("Qual a primeira nota? "))
+nota2=int(input("Qual a segunda nota? "))
+nota3=int(input("Qual a terceira nota? "))
+print("Sua média é ", ((nota1)+(nota2)+(nota3))/3)
+
+#QUESTÃO 12
+ano_atual=int(input("Qual o ano atual? "))
+ano_nascimento=int(input("Qual o ano de nascimento? "))
+print("Sua idade é ", (ano_atual)-(ano_nascimento))
+
+#QUESTÃO 13
+peso=int(input("Qual seu peso? "))
+altura=float(input("Qual seu altura? "))
+print("Seu IMC é ", (peso)/(altura**2))
+
+#QUESTÃO 14
+dias=int(input("Quantos dias se passaram? "))
+print((dias), "equivalem a", ((dias)*24), "horas")
+
+#QUESTÃO 15
+produto=str(input("Qual produto você escolhe? "))
+valor=float(input("Qual valor do produto? "))
+print("O preço do", (produto), "é", ((valor) - ((valor) *0.10)))
+
+#QUESTÃO 16
+numero1=int(input("Escolha o pirmieor número "))
+numero2=int(input("Escolha o segundo número "))
+if numero1 > numero2:
+    print((numero1), "é maior")
+elif numero2 > numero1:
+    print(numero2, "é maior")
+
+#QUESTÃO 17
+numero1=int(input("Escolha o pirmieor número "))
+numero2=int(input("Escolha o segundo número "))
+if numero1 < numero2:
+    print((numero1), "é menor")
+elif numero2 < numero1:
+    print(numero2, "é menor10")
+
+#QUESTÃO 18
+numero1=int(input("Escolha o pirmieor número "))
+numero2=int(input("Escolha o segundo número "))
+numero3=int(input("Escolha o terceiro número "))
+soma = numero1+numero2+numero3
+print("A soma dos numeros é ", (soma))
+
+#QUESTÃO 19
+numero1=int(input("Qual o numero  1? "))
+numero2=int(input("Qual o numero  2? "))
+numero3=int(input("Qual o numero 3? "))
+print("A média dos numeoros é ", ((numero1)+(numero2)+(numero3))/3)
+
+#QUESTÃO 20
+numero=int(input("Escolha um número "))
+if numero % 2 == 0:
+    print("O numero é PAR")
+else:
+    print("O número é IMPAR")
+
+#QUESTÃO 21
+time="Campinense Clube"
+jogador="Pantera"
+print("O jogador ",(jogador), "joga no ",(time))
+
+#QUESTÃO 22
+livro="Olhos Vazios"
+print("Estou lendo o livro ", (livro))
+
+#QUESTÃO 23
+profissão='vigilhante'
+empresa='Prefeitura de Campina Grande'
+print('Trabalho como ', (profissão), "na", (empresa))
+
+#QUESTÃO 24
+cor=input('Qual sua cor favorita? ')
+print('Minha cor favorita é ',(cor))
+
+#QUESTÃO 25
+animal=input('Qual seu animal favorito? ')
+nome=input('Qual o nome do seu animal? ')
+print("Meu animal de estimação é", (animal), 'e ele se chama', (nome))
+
+#QUESTÃO 26
+nome=input("Qual seu nome? ")
+comida=input('Qual sua comida favorita? ')
+print("Meu nome é ",(nome),"e gosto de comer", (comida))
+
+#QUESTÃO 27
+cantor=input('Esclolha um cantor')
+musica=input('Qual musica esse cantor canta? ')
+print("O cantor",(cantor),'canta a música',(musica))
+
+#QUESTÃO 28
+cidade=input("Qual cidade você já vititou? ")
+ano=input('Qual doi o ano da vísita')
+print('Visitei',(cidade),'em', (ano))
+
+#QUESTÃO 29
+escola=input('Qual o nome da escola que você estuda? ')
+curso=input('Qual curso você faz nessa escola? ')
+print('Estudo na escola',(escola),'e faço o curso de',(curso))
+
+#QUESTÃO 30
+amigo=input('Qual o nome do seu amigo? ')
+apelido=input("Qual o apelido do seu amigo? ")
+print("Meu amigo",(amigo),'também é conhecido por', (apelido))
+
 
 
